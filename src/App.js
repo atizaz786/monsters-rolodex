b123bca26ea943125e24728c1d1327b6e0dad60e
atizaz786 <atizazhashmi786@gmail.com>
2023-10-14 20:29:33 +0100

    feat: Added sort option by name for cards

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Card from './components/card.component';
 function App() {
   const [monsters, setMonsters] = useState([])
   const [searchField, setSearchField] = useState('')
+  const [sortation, setSortation] = useState(false)
 
   const handleChange = (e) => { setSearchField(e.target.value) }
 
@@ -20,14 +21,31 @@ function App() {
       })
   }, [])
 
+ const sortedMonsters = () => {
+  if (sortation) {
+     return filteredMonsters.sort((a,b) => a.name - b.name); // b - a for reverse sort
+  }
+  else if (!sortation) {
+    return filteredMonsters.sort((a,b) => b.name - a.name); // b - a for reverse sort
+  }
+  else {
+    return filteredMonsters;
+  }
+
+ }
+
+ const buttonClick = () => {
+   setSortation(!sortation)
+   console.log(sortation)
+  }
+
 
   return (
     <div className="App bg-gray-100 bg-gradient">
       <h1 className='font-bold text-6xl justify-center creepster'> Monsters Rolodex </h1>
-      <input className='w-1/2 mx-auto mt-4 p-2 rounded-md shadow-md' type='search' placeholder='Search Monsters' onChange={handleChange} />
-      <Card monsters={filteredMonsters} />
-
-
+      <input className='w-1/3 mx-auto mt-4 p-2 rounded-md shadow-md' type='search' placeholder='Search Monsters' onChange={handleChange} />
+      <button className='w-1/3 mx-auto mt-4 p-2 ml-3 rounded-md shadow-md' onClick={buttonClick}>Sort Monsters</button>
+      <Card monsters={sortation ? filteredMonsters.sort((a,b) => a.name - b.name) : filteredMonsters.sort((a,b) => b.name - a.name) } />
     </div>
   );
 }
--- src/components/card.component.jsx
+++ src/components/card.component.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Card(props) {
     const { monsters } = props;
+    console.log(monsters)
     return (
         <div>
             {monsters && (<div className='m-2 flex-wrap flex mt-4 justify-evenly'> {monsters.map(monster => (
-                <div className='max-w-sm ml-3 mb-3 p-8 bg-color rounded-md overflow-hidden shadow-sm basis-1/3 transition transform hover:scale-105 drop-shadow-md cursor-pointer '>
+                <div key={monster?.id} className='max-w-sm ml-3 mb-3 p-8 bg-color rounded-md overflow-hidden shadow-sm basis-1/3 transition transform hover:scale-105 drop-shadow-md cursor-pointer '>
                     <img className='w-32 h-32 mx-auto rounded-full' src={`https://robohash.org/${monster?.id}?set=set2&size=180x180`} alt={`monster ${monster?.name}`} />
                     <h2 className='text-xl font-bold text-center mt-4 inline'>{monster?.name}</h2>
                     <p>{monster?.company?.catchPhrase}</p>
